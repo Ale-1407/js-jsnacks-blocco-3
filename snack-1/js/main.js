bd028db500f3d9182a4022a58960cee2dd1472ef
Ale-1407 <alessiacarrera07@gmail.com>
2022-10-26 17:02:05 +0200

    completato snack-1

--- snack-1/js/main.js
+++ snack-1/js/main.js
@@ -9,8 +9,18 @@ fino a quando la somma degli elementi è minore di 50.
 //creo array vuoto
 let number = [];
 
-//chiedo all'utente un numero per l'array
-let numberUser = parseInt(prompt('Inserisci un numero'));
-number.push(numberUser);
-console.log(number);
+//creo un ciclo while per chiedere numeri all'utente finchè 
+//la loro somma non è minore di 50
+let sum = 0;
+while ( sum < 50){
+    //chiedo il numero
+    let numberUser = parseInt(prompt('Inserisci un numero'));
+    //sommo i numeri
+    sum += numberUser;
+    //aggiungo il numero dell'utente all'array
+    number.push(numberUser);
+    
+}
 
+console.log(number);
+console.log(`La somma è: ${sum}`);
