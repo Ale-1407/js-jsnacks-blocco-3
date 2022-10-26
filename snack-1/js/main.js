c74b5c0eccf5589360f40aa3b5faf60ee049c4b1
Ale-1407 <alessiacarrera07@gmail.com>
2022-10-26 16:32:43 +0200

    creato array, prompt per numero e push all'array

--- snack-1/js/main.js
+++ snack-1/js/main.js
@@ -5,3 +5,12 @@ da inserire nell’array.
 Continua a chiedere i numeri all’utente e a inserirli nell’array 
 fino a quando la somma degli elementi è minore di 50.
 */
+
+//creo array vuoto
+let number = [];
+
+//chiedo all'utente un numero per l'array
+let numberUser = parseInt(prompt('Inserisci un numero'));
+number.push(numberUser);
+console.log(number);
+
